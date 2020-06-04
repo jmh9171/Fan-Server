b8824a071e63439a40660278ccc8b8b3f7796ff9
Jacob Heylek <45016897+jmh9171@users.noreply.github.com>
2020-06-04 13:46:16 -0400

    Add files via upload

--- TempControl.py
+++ TempControl.py
@@ -1,6 +1,7 @@
 import threading
 import scripts
 import time
+import http.client
 
 
 class myThread (threading.Thread):
@@ -10,6 +11,21 @@ class myThread (threading.Thread):
 
     def getTemp(self):
         # This function will have access to the temp sensors and be able to see the real-time temp
+
+
+        conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
+
+        headers = {
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+            'x-rapidapi-key': "07af810bbbmsh617ce2e002ce378p1cbdbcjsnac1c945c0cba"
+            }
+
+        conn.request("GET", "/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=Lockhaven", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+
+        print(data.decode("utf-8"))
+
         self.temp = self.temp + 2
         return self.temp
 
@@ -29,6 +45,9 @@ class myThread (threading.Thread):
             info = scripts.getJsonData('data.json')
             info['currentTemp'] = str(self.getTemp())
             scripts.handlePut('data',info)
+            
+
+
                 
 
 
